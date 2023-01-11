3695eb806145a8ae1494a2945a6a1e4b47db15a5
ege oguzman <eoguzman@amazon.com>
2023-01-11 21:10:44 +0000

    second commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Amplify } from "aws-amplify";
+
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Header } from "./Components/General/Header";
+import { Footer } from "./Components/General/Footer";
+import { SignInHeader } from "./Components/Login/SignInHeader";
+import { SignInFooter } from "./Components/Login/SignInFooter";
+import "./styles.css";
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+export function App({ signOut, user }) {
+  return (
+    <main>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </main>
+  );
+}
+
+export default withAuthenticator(App, {
+  components: {
+    Header,
+    SignIn: {
+      Header: SignInHeader,
+      Footer: SignInFooter
+    },
+    Footer
+  }
+});
